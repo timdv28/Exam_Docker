9dcddcc8ea820445cd674f22a31a7f2fe54dafdd
Tim <timdv28@hotmail.com>
2025-06-02 07:31:57 +0000

    Commited the first versions of the test and compose files

---
+++ api_test.py
@@ -0,0 +1,37 @@
+import os
+import requests
+# definition of the API address
+api_address = '127.0.0.1'
+# API port
+api_port = 8000
+# requête
+r = requests.get(
+    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland'
+    }
+)
+output = '''
+============================
+    Authentication test
+============================
+request done at "/permissions"
+| username="alice"
+| password="wonderland"
+expected result = 200
+actual restult = {status_code}
+==>  {test_status}
+'''
+# query status
+status_code = r.status_code
+# display the results
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+# printing in a file
+if os.environ.get('LOG') == 1:
+    with open('api_test.log', 'a') as file:
+        file.write(output)
